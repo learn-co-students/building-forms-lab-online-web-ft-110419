542dcfb3a4bfa1b20bd0e599623457b133fb725e
jessesbyers <jesse.s.byers@gmail.com>
2020-04-01 15:09:07 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,54 @@
-// Add BandInput component
 import React, { Component } from 'react'
 
+
 class BandInput extends Component {
+
+  state = {
+    name: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    console.log(this)
+    event.preventDefault()
+    let band = {name: this.state.name}
+    this.props.addBand(band)
+    this.setState({
+      name: ''
+    })
+  }
+
   render() {
+    console.log(this.props)
     return(
-      <div>
-        Band Input
+      <div onSubmit={this.handleSubmit}>
+      {/* <div onSubmit={event => this.props.addBand({name: this.state.name})}> */}
+
+        <form>
+          <input name="name" type="text" value={this.state.name} onChange={this.handleChange}></input>
+          <button type="submit" >Submit</button>
+        </form>
       </div>
     )
   }
 }
+
+// const mapStateToProps = (state) => {
+//   return { state }
+// }
+
+// const mapDispatchToProps = (dispatch) => {
+//   console.log(dispatch)
+//   return {
+//     addBand: (band) => {
+//       dispatch(this.props.addBand(band))
+//     }
+//   }
+// }
 
 export default BandInput
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import BandInput from '../components/BandInput'
+
 
 class BandsContainer extends Component {
+
   render() {
+    console.log(this.props.state.bands)
+    console.log(this)
     return(
       <div>
-        BandsContainer
+        {<BandInput addBand={this.props.addBand}/>}
+        <ul>
+          {this.props.state.bands.map(band => {
+            return (
+            <li>{band.name}</li>
+            )}
+          )}
+        </ul>
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = (state) => {
+  return { state }
+}
+
+const addBand = (band) => {
+  return {
+    type: 'ADD_BAND',
+    band
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addBand: (band) => {
+      dispatch(addBand(band))
+    }
+  };
+}
+
+// export default connect (mapStateToProps, {addBand})(BandsContainer)
+export default connect (mapStateToProps, mapDispatchToProps)(BandsContainer)
+
