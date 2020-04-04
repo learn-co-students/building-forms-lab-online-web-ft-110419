5afb1ccf72ff68be0692db7c8ff38ea672154c99
BrandonMWeaver <a7xncob@gmail.com>
2020-04-04 13:08:00 -0400

    Done.

---
+++ src/components/Bands.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Bands = props => {
+	return (
+		<div>
+			{props.bands.map((band, index) => {
+				return (
+					<li key={index}>{band.name}</li>
+				);
+			})}
+		</div>
+	);
+}
+
+export default Bands;
